691b89775d488bc5316efc9c1b627f7411519182
Connor Boyd <Connor.Boyd@spir3.com>
2013-04-15 15:47:29 -0600

    Initial commit. Adding all files

---
+++ Treasure/src/Game.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Game {
+	
+	private int numKeys, numChests;
+	private List<Integer> myKeys;
+	private List<Chest> chestList;
+	
+	public Game(List<Integer> keys, List<Chest> chests)
+	{
+		myKeys = keys;
+		chestList = chests;
+	}
+	
+	
+	
+}
